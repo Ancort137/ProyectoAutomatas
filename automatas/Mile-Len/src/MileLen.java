59c24c53bb7b43f6797f7b1613fdf1067f552858
csc1234 <cristobalcesar.sanchez@gmail.com>
2018-05-07 14:19:37 -0300

    Se ha subido la carpeta que contiene el proyecto del profesor y el nuestro. Recordar despues separar los proyectos.

---
+++ automatas/Mile-Len/src/MileLen.java
@@ -0,0 +1,28 @@
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class MileLen {
+	private static String load(String preCodeFile) 
+			  throws java.io.IOException {
+		return new String(
+				java.nio.file.Files.readAllBytes(
+						java.nio.file.Paths.get(preCodeFile)), 
+				java.nio.charset.Charset.forName("UTF-8"));
+	}
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("Scanning file...");
+			ANTLRInputStream input = new ANTLRInputStream (
+					load("examples/eje1.mile")) ;
+			MileParserLexer lexer = new MileParserLexer(input);
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
+			MileParserParser parser = new MileParserParser(tokens);
+			ParseTree tree = parser.program();
+			System.out.println("Done");
+		} catch (Exception e) {
+			System.err.print(e);
+		}
+	}
+}
---
+++ automatas/Mile-Par/MileLexer.g4
@@ -0,0 +1,49 @@
+lexer grammar MileLexer;
+
+fragment TRUE	: 'accept'			;
+fragment FALSE	: 'decline'			;
+fragment NAT	: [0-9]				;
+fragment DOT	: '.'				;
+
+ 
+PLUS 	: '+'	;
+MINUS	: '-'	;
+AND		: '$$'	;
+OR		: '//'	;
+EQUAL	: '='	;
+NQUAL	: '<>'	;
+GT		: '>'	;
+LT		: '<'	;
+ASSIGN 	: '<->'	; // ********
+LPAR	: '('	;
+RPAR	: ')'	;
+LBRACE	: '{'	;
+RBRACE	: '}'	;
+
+BEGIN_RW	: 'born'		;
+END_RW		: 'die'			;
+IF_RW		: 'attack'		;
+ELSE_RW		: 'defend'		;
+//ENDIF_RW	: 'fin_si'		;
+READ_RW		: 'listen'		;
+WRITE_RW	: 'talk'		;
+INTEGER_RW	: 'mage'		;
+STRING_RW	: 'knigth'		;
+FLOAT_RW	: 'priest'		;
+//REAL_RW	: 'real'		;
+//BOOLEAN_RW: 'logico'		;
+
+NUMBER	: NAT+				;
+FLOAT 	: NUMBER DOT NUMBER	;
+BOOLEAN 	: TRUE | FALSE	;
+STRING 	: '"' .*? '"'		;
+
+
+
+ID		: [a-zA-Z]+						;
+COMMENT	:  '||' .*? '\r'? '\n' -> skip	;
+WS 		: [ \t\r\n]+ -> skip 			;
+
+
+
+
